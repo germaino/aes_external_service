f20b46aa1de19ee9c0d02e6d34b54fd93d377a5f
Olivier GERMAIN <olivier.germain@schneider-electric.com>
2023-12-03 23:28:04 +0100

    base64 encode cert

--- v3_Auth/main.go
+++ v3_Auth/main.go
@@ -9,8 +9,8 @@ import (
 	"log"
 	"net/http"
 	"net/url"
-	"strconv"
-	"time"
+//	"strconv"
+//	"time"
 
 	"github.com/golang/protobuf/ptypes/wrappers"
 	"google.golang.org/genproto/googleapis/rpc/code"
@@ -22,6 +22,11 @@ import (
 	envoyType "github.com/datawire/ambassador/v2/pkg/api/envoy/type/v3"
 
 	"github.com/datawire/dlib/dhttp"
+//        "encoding/json"
+//	"fmt"
+	"strings"
+        "encoding/base64"
+        "regexp"
 )
 
 func main() {
@@ -67,54 +72,92 @@ func (s *AuthService) Check(ctx context.Context, req *envoyAuthV3.CheckRequest) 
 	log.Println("RequestURI: ", requestURI)
 
 	// Read over and log the headers for the request
-	denyHeader := false
+	hasCert := false
+        certInfo := make(map[string]string)
 	log.Println("|~~~~~~~~~~~~ BEGIN HEADERS ~~~~~~~~~~~~|")
 	for k, v := range req.GetAttributes().GetRequest().GetHttp().GetHeaders() {
 		log.Printf("%s: %s", k, v)
 		// Sleep for x seconds when this header is present
-		if k == "sleepfor" {
-			seconds, _ := strconv.Atoi(v)
-			log.Printf("%s%d%s", "Sleeping for ", seconds, " seconds...")
-			time.Sleep(time.Duration(seconds) * time.Second)
-		} else if k == "deny-me" {
-			denyHeader = true
-		}
+		if k == "x-forwarded-client-cert" {
+                  hasCert = true
+                  log.Println("Found header x-forwarded-client-cert")
+                  entries := strings.Split(v, ";")
+	          for _, e := range entries {
+		    parts := strings.SplitN(e, "=", 2)
+                    elem := parts[1]
+		    if len(elem) > 0 && elem[0] == '"' {
+			elem = elem[1:]
+		    }
+		    if len(elem) > 0 && elem[len(elem)-1] == '"' {
+			elem = elem[:len(elem)-1]
+		    }
+                    parts[1] = elem
+		    certInfo[parts[0]] = parts[1]
+                    if parts[0] == "Cert" {
+			//fmt.Println(url.QueryUnescape(certInfo[parts[0]]))
+			certPem, _ := url.QueryUnescape(certInfo[parts[0]])
+			certInfo[parts[0]] = base64.StdEncoding.EncodeToString([]byte(certPem))
+		    }
+	          }
+                  //jsonString, _ := json.Marshal(certInfo)
+	          //fmt.Println(string(jsonString))
+                }
 	}
 	log.Println("|~~~~~~~~~~~~ END HEADERS ~~~~~~~~~~~~|")
 
-	if requestURI.Path == "/deny-me/" || denyHeader {
-		log.Println("=> DENIED REQUEST", err)
-		return &envoyAuthV3.CheckResponse{
-			Status: &status.Status{Code: int32(code.Code_PERMISSION_DENIED)},
-			HttpResponse: &envoyAuthV3.CheckResponse_DeniedResponse{
-				DeniedResponse: &envoyAuthV3.DeniedHttpResponse{
-					Status: &envoyType.HttpStatus{Code: http.StatusForbidden},
-					Headers: []*envoyCoreV3.HeaderValueOption{
-						{Header: &envoyCoreV3.HeaderValue{Key: "Content-Type", Value: "application/json"}},
-					},
-					Body: `{"msg": "Your request was denied, unauthorized path /deny-me/"}`,
-				},
-			},
-		}, nil
+        match_est, _ := regexp.MatchString(".well-known/est", requestURI.Path)
+        match_bootstrap, _ := regexp.MatchString("/api/bootstrap", requestURI.Path)
+        match_quote, _ := regexp.MatchString("/quote", requestURI.Path)
+
+	if (match_bootstrap || match_est || match_quote ) && ! hasCert {
+	  log.Println("=> DENIED REQUEST", err)
+          return &envoyAuthV3.CheckResponse{
+	    Status: &status.Status{Code: int32(code.Code_PERMISSION_DENIED)},
+	    HttpResponse: &envoyAuthV3.CheckResponse_DeniedResponse{
+	      DeniedResponse: &envoyAuthV3.DeniedHttpResponse{
+	        Status: &envoyType.HttpStatus{Code: http.StatusForbidden},
+		Headers: []*envoyCoreV3.HeaderValueOption{
+		  {Header: &envoyCoreV3.HeaderValue{Key: "Content-Type", Value: "application/json"}},
+		},
+		Body: `{"msg": "Your request was denied, unauthorized"}`,
+	      },
+	    },
+	  }, nil
 	}
 
 	log.Print("=> ALLOW REQUEST")
-	return &envoyAuthV3.CheckResponse{
-		Status: &status.Status{Code: int32(code.Code_OK)},
-		HttpResponse: &envoyAuthV3.CheckResponse_OkResponse{
-			OkResponse: &envoyAuthV3.OkHttpResponse{
-				Headers: []*envoyCoreV3.HeaderValueOption{
-					{
-						Header: &envoyCoreV3.HeaderValue{Key: "V3AlphaOverwrite", Value: "HeaderOverwritten"},
-						Append: &wrappers.BoolValue{Value: false},
-					},
-					{
-						Header: &envoyCoreV3.HeaderValue{Key: "V3AlphaAppend", Value: "HeaderAppended"},
-						Append: &wrappers.BoolValue{Value: true},
-					},
-				},
-			},
+        if (hasCert) {
+	  return &envoyAuthV3.CheckResponse{
+	    Status: &status.Status{Code: int32(code.Code_OK)},
+	      HttpResponse: &envoyAuthV3.CheckResponse_OkResponse{
+	        OkResponse: &envoyAuthV3.OkHttpResponse{
+		  Headers: []*envoyCoreV3.HeaderValueOption{
+		    {
+		      Header: &envoyCoreV3.HeaderValue{Key: "X-ARR-ClientCert", Value: certInfo["Cert"]},
+		      Append: &wrappers.BoolValue{Value: false},
+		    },
+		    {
+		      Header: &envoyCoreV3.HeaderValue{Key: "Content-Type", Value: "application/pkcs10"},
+		      Append: &wrappers.BoolValue{Value: false},
+		    },
+                    {
+		      Header: &envoyCoreV3.HeaderValue{Key: "Content-Transfer-Encoding", Value: "base64"},
+		      Append: &wrappers.BoolValue{Value: false},
+		    },
+		  },
 		},
-	}, nil
+	      },
+	  }, nil
+        }
+
+        return &envoyAuthV3.CheckResponse{
+          Status: &status.Status{Code: int32(code.Code_OK)},
+          HttpResponse: &envoyAuthV3.CheckResponse_OkResponse{
+            OkResponse: &envoyAuthV3.OkHttpResponse{
+              Headers: []*envoyCoreV3.HeaderValueOption{
+              },
+            },
+          },
+        }, nil
 
 }
